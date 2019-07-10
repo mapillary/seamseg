0ca8bc9c1c16e7229fe07be0e22ded791dfc7e4a
Lorenzo Porzi <lorenzo@mapillary.com>
2019-07-10 16:57:45 +0200

    Data preparation and utility scripts

---
+++ scripts/data_preparation/prepare_cityscapes.py
@@ -0,0 +1,230 @@
+import argparse
+import glob
+import json
+import shutil
+from multiprocessing import Pool, Value, Lock
+from os import path, mkdir, listdir
+
+import numpy as np
+import tqdm
+import umsgpack
+from PIL import Image
+from cityscapesscripts.helpers.labels import labels as cs_labels
+from pycococreatortools import pycococreatortools as pct
+
+parser = argparse.ArgumentParser(description="Convert Cityscapes to seamseg format")
+parser.add_argument("root_dir", metavar="ROOT_DIR", type=str, help="Root directory of Vistas")
+parser.add_argument("out_dir", metavar="OUT_DIR", type=str, help="Output directory")
+
+_SPLITS = {
+    "train": ("leftImg8bit/train", "gtFine/train"),
+    "val": ("leftImg8bit/val", "gtFine/val"),
+    "coarse": ("leftImg8bit/train_extra", "gtCoarse/train_extra")
+}
+_INSTANCE_EXT = "_instanceIds.png"
+_IMAGE_EXT = "_leftImg8bit.png"
+
+
+def main(args):
+    print("Loading Cityscapes from", args.root_dir)
+    num_stuff, num_thing = _get_meta()
+
+    # Prepare directories
+    img_dir = path.join(args.out_dir, "img")
+    _ensure_dir(img_dir)
+    msk_dir = path.join(args.out_dir, "msk")
+    _ensure_dir(msk_dir)
+    lst_dir = path.join(args.out_dir, "lst")
+    _ensure_dir(lst_dir)
+    coco_dir = path.join(args.out_dir, "coco")
+    _ensure_dir(coco_dir)
+
+    # COCO-style category list
+    coco_categories = []
+    for lbl in cs_labels:
+        if lbl.trainId != 255 and lbl.trainId != -1 and lbl.hasInstances:
+            coco_categories.append({
+                "id": lbl.trainId,
+                "name": lbl.name
+            })
+
+    # Process splits
+    images = []
+    for split, (split_img_subdir, split_msk_subdir) in _SPLITS.items():
+        print("Converting", split, "...")
+
+        img_base_dir = path.join(args.root_dir, split_img_subdir)
+        msk_base_dir = path.join(args.root_dir, split_msk_subdir)
+        img_list = _get_images(msk_base_dir)
+
+        # Write the list file
+        with open(path.join(lst_dir, split + ".txt"), "w") as fid:
+            fid.writelines(img_id + "\n" for _, img_id, _ in img_list)
+
+        # Convert to COCO detection format
+        coco_out = {
+            "info": {"version": "1.0"},
+            "images": [],
+            "categories": coco_categories,
+            "annotations": []
+        }
+
+        # Process images in parallel
+        worker = _Worker(img_base_dir, msk_base_dir, img_dir, msk_dir)
+        with Pool(initializer=_init_counter, initargs=(_Counter(0),)) as pool:
+            total = len(img_list)
+            for img_meta, coco_img, coco_ann in tqdm.tqdm(pool.imap(worker, img_list, 8), total=total):
+                images.append(img_meta)
+
+                # COCO annotation
+                coco_out["images"].append(coco_img)
+                coco_out["annotations"] += coco_ann
+
+        # Write COCO detection format annotation
+        with open(path.join(coco_dir, split + ".json"), "w") as fid:
+            json.dump(coco_out, fid)
+
+    # Write meta-data
+    print("Writing meta-data")
+    meta = {
+        "images": images,
+        "meta": {
+            "num_stuff": num_stuff,
+            "num_thing": num_thing,
+            "categories": [],
+            "palette": [],
+            "original_ids": []
+        }
+    }
+
+    for lbl in cs_labels:
+        if lbl.trainId != 255 and lbl.trainId != -1:
+            meta["meta"]["categories"].append(lbl.name)
+            meta["meta"]["palette"].append(lbl.color)
+            meta["meta"]["original_ids"].append(lbl.id)
+
+    with open(path.join(args.out_dir, "metadata.bin"), "wb") as fid:
+        umsgpack.dump(meta, fid, encoding="utf-8")
+
+
+def _get_images(base_dir):
+    img_list = []
+    for subdir in listdir(base_dir):
+        subdir_abs = path.join(base_dir, subdir)
+        if path.isdir(subdir_abs):
+            for img in glob.glob(path.join(subdir_abs, "*" + _INSTANCE_EXT)):
+                _, img = path.split(img)
+
+                parts = img.split("_")
+                img_id = "_".join(parts[:-2])
+                lbl_cat = parts[-2]
+
+                img_list.append((subdir, img_id, lbl_cat))
+
+    return img_list
+
+
+def _get_meta():
+    num_stuff = sum(1 for lbl in cs_labels if 0 <= lbl.trainId < 255 and not lbl.hasInstances)
+    num_thing = sum(1 for lbl in cs_labels if 0 <= lbl.trainId < 255 and lbl.hasInstances)
+    return num_stuff, num_thing
+
+
+def _ensure_dir(dir_path):
+    try:
+        mkdir(dir_path)
+    except FileExistsError:
+        pass
+
+
+class _Worker:
+    def __init__(self, img_base_dir, msk_base_dir, img_dir, msk_dir):
+        self.img_base_dir = img_base_dir
+        self.msk_base_dir = msk_base_dir
+        self.img_dir = img_dir
+        self.msk_dir = msk_dir
+
+    def __call__(self, img_desc):
+        img_dir, img_id, lbl_cat = img_desc
+        coco_ann = []
+
+        # Load the annotation
+        with Image.open(path.join(self.msk_base_dir, img_dir, img_id + "_" + lbl_cat + _INSTANCE_EXT)) as lbl_img:
+            lbl = np.array(lbl_img)
+            lbl_size = lbl_img.size
+
+        ids = np.unique(lbl)
+
+        # Compress the labels and compute cat
+        lbl_out = np.zeros(lbl.shape, np.int32)
+        cat = [255]
+        iscrowd = [0]
+        for city_id in ids:
+            if city_id < 1000:
+                # Stuff or group
+                cls_i = city_id
+                iscrowd_i = cs_labels[cls_i].hasInstances
+            else:
+                # Instance
+                cls_i = city_id // 1000
+                iscrowd_i = False
+
+            # If it's a void class just skip it
+            if cs_labels[cls_i].trainId == 255 or cs_labels[cls_i].trainId == -1:
+                continue
+
+            # Extract all necessary information
+            iss_class_id = cs_labels[cls_i].trainId
+            iss_instance_id = len(cat)
+            mask_i = lbl == city_id
+
+            # Save ISS format annotation
+            cat.append(iss_class_id)
+            iscrowd.append(1 if iscrowd_i else 0)
+            lbl_out[mask_i] = iss_instance_id
+
+            # Compute COCO detection format annotation
+            if cs_labels[cls_i].hasInstances:
+                category_info = {"id": iss_class_id, "is_crowd": iscrowd_i}
+                coco_ann_i = pct.create_annotation_info(
+                    counter.increment(), img_id, category_info, mask_i, lbl_size, tolerance=2)
+                if coco_ann_i is not None:
+                    coco_ann.append(coco_ann_i)
+
+        # COCO detection format image annotation
+        coco_img = pct.create_image_info(img_id, path.join(img_dir, img_id + _IMAGE_EXT), lbl_size)
+
+        # Write output
+        Image.fromarray(lbl_out).save(path.join(self.msk_dir, img_id + ".png"))
+        shutil.copy(path.join(self.img_base_dir, img_dir, img_id + _IMAGE_EXT),
+                    path.join(self.img_dir, img_id + ".png"))
+
+        img_meta = {
+            "id": img_id,
+            "cat": cat,
+            "size": (lbl_size[1], lbl_size[0]),
+            "iscrowd": iscrowd
+        }
+
+        return img_meta, coco_img, coco_ann
+
+
+def _init_counter(c):
+    global counter
+    counter = c
+
+
+class _Counter:
+    def __init__(self, initval=0):
+        self.val = Value('i', initval)
+        self.lock = Lock()
+
+    def increment(self):
+        with self.lock:
+            val = self.val.value
+            self.val.value += 1
+        return val
+
+
+if __name__ == "__main__":
+    main(parser.parse_args())
---
+++ scripts/utility/convert_pytorch_resnet.py
@@ -0,0 +1,77 @@
+import argparse
+
+import torch
+import torch.utils.model_zoo as model_zoo
+
+MODEL_URLS = {
+    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+}
+
+NETS = {
+    "resnet18": {"structure": [2, 2, 2, 2], "bottleneck": False},
+    "resnet34": {"structure": [3, 4, 6, 3], "bottleneck": False},
+    "resnet50": {"structure": [3, 4, 6, 3], "bottleneck": True},
+    "resnet101": {"structure": [3, 4, 23, 3], "bottleneck": True},
+    "resnet152": {"structure": [3, 8, 36, 3], "bottleneck": True},
+}
+
+CONV_PARAMS = ["weight"]
+BN_PARAMS = ["weight", "bias", "running_mean", "running_var"]
+
+parser = argparse.ArgumentParser(description="Convert pre-trained ResNet from Pytorch to our format")
+parser.add_argument("net", metavar="NET", type=str, choices=list(MODEL_URLS.keys()), help="Network name")
+parser.add_argument("out_file", metavar="OUT", type=str, help="Output file")
+
+
+def copy_layer(inm, outm, name_in, name_out, params):
+    for param_name in params:
+        outm[name_out + "." + param_name] = inm[name_in + "." + param_name]
+
+
+def convert(model, structure, bottleneck):
+    out = dict()
+    num_convs = 3 if bottleneck else 2
+
+    # Initial module
+    copy_layer(model, out, "conv1", "mod1.conv1", CONV_PARAMS)
+    copy_layer(model, out, "bn1", "mod1.bn1", BN_PARAMS)
+
+    # Other modules
+    for mod_id, num in enumerate(structure):
+        for block_id in range(num):
+            for conv_id in range(num_convs):
+                copy_layer(model, out,
+                           "layer{}.{}.conv{}".format(mod_id + 1, block_id, conv_id + 1),
+                           "mod{}.block{}.convs.conv{}".format(mod_id + 2, block_id + 1, conv_id + 1),
+                           CONV_PARAMS)
+                copy_layer(model, out,
+                           "layer{}.{}.bn{}".format(mod_id + 1, block_id, conv_id + 1),
+                           "mod{}.block{}.convs.bn{}".format(mod_id + 2, block_id + 1, conv_id + 1),
+                           BN_PARAMS)
+
+            # Try copying projection module
+            try:
+                copy_layer(model, out,
+                           "layer{}.{}.downsample.0".format(mod_id + 1, block_id),
+                           "mod{}.block{}.proj_conv".format(mod_id + 2, block_id + 1),
+                           CONV_PARAMS)
+                copy_layer(model, out,
+                           "layer{}.{}.downsample.1".format(mod_id + 1, block_id),
+                           "mod{}.block{}.proj_bn".format(mod_id + 2, block_id + 1),
+                           BN_PARAMS)
+            except KeyError:
+                pass
+
+    return out
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    original = model_zoo.load_url(MODEL_URLS[args.net])
+    converted = convert(original, **NETS[args.net])
+    torch.save(converted, args.out_file)
